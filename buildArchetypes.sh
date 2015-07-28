5d46d1145bff0390a13fe0cd29979a9e65f292a1
Simon Matic Langford <simon@exemel.co.uk>
2015-07-28 07:39:37 +0100

    Test archetypes as part of build

--- buildArchetypes.sh
+++ buildArchetypes.sh
@@ -5,8 +5,14 @@ set -e
 for i in `ls`; do
   if [[ -d $i ]]; then
     cd $i
+    ARCHETYPE_VERSION=`cat pom.xml | grep version | grep SNAPSHOT | sed -e 's/[\<\>]/ /g' | awk '{print $2}'`
     mvn install
     mvn install -B -N -Parchetype
+    cd ..
+    mvn archetype:generate -DarchetypeGroupId=com.betfair.cougar.archetypes -DarchetypeArtifactId=simple-service-archetype -DarchetypeVersion=$ARCHETYPE_VERSION -DgroupId=wibble -DartifactId=wibble -Dversion=1.0-SNAPSHOT -Dpackage=wibble -B
+    cd wibble
+    mvn -Prename -Dupper=Wibble -Dlower=wibble install -N 
+    mvn install
     cd ..
   fi
 done 
